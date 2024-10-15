578a0e0ba6d20280be6de3d293d001a82d321c70
AyushiBadika <badikaayushi@gmail.com>
2024-10-16 00:45:06 +0530

    corrected wishlist empty image size

--- ecommerce-website/src/components/Wishlist.jsx
+++ ecommerce-website/src/components/Wishlist.jsx
@@ -10,22 +10,12 @@ export default function Wishlist() {
       {wishlist?.length > 0 ? (
         <div className="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))]  md:grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 px-8 sm:px-20 md:gap-8 m-auto ">
           {wishlist.map((product, index) => (
-            <ProductCard
-              key={index}
-              imgUrl={product?.images[0]}
-              productName={product.title}
-              rating={product.rating}
-              noOfRating={product.noOfReviews}
-              mrp={product?.pricing?.mrp}
-              cost={product?.pricing?.cost}
-              id={product.id}
-              discount={product.pricing.discount}
-            />
+            <ProductCard key={index} imgUrl={product?.images[0]} productName={product.title} rating={product.rating} noOfRating={product.noOfReviews} mrp={product?.pricing?.mrp} cost={product?.pricing?.cost} id={product.id} discount={product.pricing.discount} />
           ))}{" "}
         </div>
       ) : (
         <div className="flex justify-center items-center flex-col w-[100vw]">
-          <img src={emptyWishlist} className="" />
+          <img src={emptyWishlist} className="w-96" />
           <div className="text-center text-3xl font-bold">Wishlist Empty!</div>
         </div>
       )}
